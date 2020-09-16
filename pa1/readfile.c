153f076ecb0a46a35ea870b8a44e67545c0bac2a
Dan Blanchette <dlblanchette@students.nic.edu>
2020-09-16 11:09:46 -0700

    pa0's read file that accepts a user designated file.

--- pa1/readfile.c
+++ pa1/readfile.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 int main()
 {
-     /* Pointer to the file */
-     FILE *fp1;
-     /* Character variable to read the content of file */
-     char c;
+    /* Pointer to the file */
+    FILE *fp1;
+    /* Character variable to read the content of file */
+    char c, fileName[100];
+    
+    printf("Please enter a file name:");
+    scanf("%s", fileName);
+    // get command line input for file name.
 
-     /* Opening a file in r mode*/
-     fp1= fopen ("hello_world.ccx", "r");
+    /* Opening a file in r mode*/
+     fp1= fopen (fileName, "r");
 
      /* Infinite loop –I have used break to come out of the loop*/
      while(1)
