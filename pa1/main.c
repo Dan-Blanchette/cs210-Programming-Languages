2fc557cfe4e340959ed33af269d4aa9ed25a137b
Danel Blanchette <dlblanchette@students.nic.edu>
2020-09-16 19:28:28 -0700

    new iteration of main.c has some of the output being printed out one line at a time.[ Is Working.]

--- pa1/main.c
+++ pa1/main.c
@@ -1,33 +1,70 @@
-/* Dan Blanchette
- * cs210 
- * Dr. Rinker
- * ( program start date: 14, sept 2020)
- * 
- * This program will identify and seperate all possible "words" 
- * as tokens from within a CCX code file. It will ignore all 
- * comment lines from the program.
- */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+ 
+// int isKeyword(char buffer[]){
+// 	char keywords[32][10] = {"auto","break","case","char","const","continue","default",
+// 							"do","double","else","enum","extern","float","for","goto",
+// 							"if","int","long","register","return","short","signed",
+// 							"sizeof","static","struct","switch","typedef","union",
+// 							"unsigned","void","volatile","while"};
+// 	int i, flag = 0;
+	
+// 	for(i = 0; i < 32; ++i){
+// 		if(strcmp(keywords[i], buffer) == 0){
+// 			flag = 1;
+// 			break;
+// 		}
+// 	}
+	
+// 	return flag;
+// }
+ 
+int main(){
+	char ch, buffer[15], operators[] = "+-*/%=", fName, uIn[255];
+	FILE *fp;
+	int i,j=0;
+	
+    // printf("Please enter a file name: ");
+    // fName = scanf("%s",uIn);
 
-#include "lex.h"
-
-int main()
-{
-    char fName[255];
-    scanf("%s",fName);
-
-    if((inFile = fopen("fName","r")) == NULL)
+	fp = fopen("hello_world","r");
+	
+	if(fp == NULL){
+		printf("error while opening the file\n");
+		exit(0);
+	}
+	
+	while((ch = fgetc(fp)) != EOF)
     {
-        printf("Error - cannot open file \n");
-    }
-    else 
-    {
-        getChar();
-        do
+
+   		
+   		if(isalnum(ch))
         {
-            lex();
-        }   while (nextToken != EOF);
-    }
-    fclose(inFile);
-    return 0;
-}
+   			buffer[j++] = ch;
+   		}
 
+        for(i = 0; i < 6; ++i)
+        {
+   			if(ch == operators[i])
+   				printf("%c\n", ch);
+   		}
+        
+   		if((ch == ' ' || ch == '_' || ch == '\n') && (j != 0))
+           {
+   				buffer[j] = '\0';
+   				j = 0;
+   				   				
+   				// if(isKeyword(buffer) == 1)
+   				// 	printf("%s is keyword\n", buffer);
+
+   					printf("%s\n", buffer);
+   		}
+   		
+	}
+	
+	fclose(fp);
+	
+	return 0;
+}
